0f211654903a203b84e31d879416a5d768d0bbf7
Expyh_from_home <exceptionalyh@gamil.com>
2021-03-04 23:59:44 +0800

    update httprequest.js

--- scripts/httprequest.js
+++ scripts/httprequest.js
@@ -1,19 +1,28 @@
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const request = require('request');
+const pd = require("node-pandas");
 
-const xhr = new XMLHttpRequest(),
-    method = "GET",
-    url = "http://yunyingdaping.fenxianglife.com/self/table?appCode=744b3a88c9974680982cc5b6612a4e92&pageNum=1&pageSize=10&start_date=2020-12-01&end_date=2020-12-02";
+const url = "http://yunyingdaping.fenxianglife.com/self/table?appCode=744b3a88c9974680982cc5b6612a4e92&pageNum=1&pageSize=10&start_date=2020-12-01&end_date=2020-12-02";
 
-xhr.open(method, url, true);
-xhr.onreadystatechange = function () {
-  // In local files, status is 0 upon success in Mozilla Firefox
-    var status = xhr.status;
-    if (this.readyState === 4 && this.status === 200) {
-      // The request has been completed successfully
-      const obj = JSON.parse(xhr.responseText);
-      console.log(obj.data.rows)
-    } else {
-      // Oh no! There has been an error with the request!
-    }
-};
-xhr.send();
+test = request(url, function getdf(error, response, body) {
+
+  const obj = JSON.parse(body);
+  var df = pd.DataFrame(obj.data.rows);
+
+  return df;
+});
+
+console.log(test.getdf())
+
+
+function creatTable(data){
+  //这个函数的参数可以是从后台传过来的也可以是从其他任何地方传过来的
+  //这里我假设这个data是一个长度为5的字符串数组 我要把他放在表格的一行里面，分成五列
+  var tableData="<tr>"
+  //动态增加5个td,并且把data数组的五个值赋给每个td
+  for(var i=0;i<data.length;i++){
+   tableData+="<td>"+data[i]+"</td>"
+  }
+  tableData+="</tr>"
+  //现在tableData已经生成好了，把他赋值给上面的tbody
+  $("#tbody1").html(tableData)
+ }
